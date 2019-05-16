51a73c8158a9e2c3efb9ccd647b8eb921f357d13
yxx <1365831278@qq.com>
2019-05-16 10:42:22 +0800

    s

--- src/config/avatar.php
+++ src/config/avatar.php
@@ -12,5 +12,5 @@ return   [
     'width' => '100',
     'height' => '100',
     'size' => '26',
-    'font_file' => public_path() . '/fonts/WawaSC-Regular.otf',
+    'font_file' => '/vendor/youxingxiang/avatar/src/fonts/1.ttf',
 ];
